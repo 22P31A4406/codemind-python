f3e75c0b24de871faf61633252f40099e9ef7af7
22P31A4406 <119730744+22P31A4406@users.noreply.github.com>
2023-05-05 12:08:37 +0530

    a,b=list(map(int,input().split())) if abs(a-b)==1:     print('Yes') elif abs(a-b)==9:     print('Yes') else:     print('No')

--- Edge_Checker.py
+++ Edge_Checker.py
@@ -1,19 +1,7 @@
-#a,b=list(map(int,input().split()))
-#if abs(a-b)==1:
- #   print('Yes')
-#elif abs(a-b)==9:
- #   print('Yes')
-#else:
- #   print('No')
-    
 a,b=list(map(int,input().split()))
-if a==b-1:
+if abs(a-b)==1:
     print('Yes')
-elif a+9==b:
-    print('Yes')
-elif a==b+1:
-    print('Yes') 
-elif a==b+9:
+elif abs(a-b)==9:
     print('Yes')
 else:
     print('No')
